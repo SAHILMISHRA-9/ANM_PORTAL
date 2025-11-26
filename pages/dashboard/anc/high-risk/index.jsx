f3c934cb53b5e06e46a612b87dfa82ce3b49c766
SAHILMISHRA-9 <sahilmishra9922@gmail.com>
2025-11-26 22:58:22 +0530

    Fixed component paths for Vercel build

--- pages/dashboard/anc/high-risk/index.jsx
+++ pages/dashboard/anc/high-risk/index.jsx
@@ -1,5 +1,5 @@
-import Sidebar from '../../../components/layout/Sidebar'
-import Navbar from '../../../components/layout/Navbar'
+import Sidebar from "../../../../components/layout/Sidebar";
+import Navbar from "../../../../components/layout/Navbar";
 
 
 export default function HighRisk() {
